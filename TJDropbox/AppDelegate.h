6c3abe91abfbec87af713cde02f0d75b883d8a3b
Tim Johnsen <timonus@gmail.com>
2016-04-11 21:23:47 -0700

    Initial Commit

---
+++ TJDropbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJDropbox
+//
+//  Created by Tim Johnsen on 4/11/16.
+//  Copyright © 2016 tijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TJDropbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJDropbox
+//
+//  Created by Tim Johnsen on 4/11/16.
+//  Copyright © 2016 tijo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
